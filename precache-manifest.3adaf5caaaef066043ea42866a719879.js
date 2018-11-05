9e7b95c263bf7aabcc1d0384bfb27c32537b3631
Noppawit Thairungroj <noppawit.trr@gmail.com>
2018-11-05 10:29:27 +0700

    first commit

---
+++ precache-manifest.3adaf5caaaef066043ea42866a719879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08d22d8dcdd4ea3dbb3f",
+    "url": "/static/js/main.08d22d8d.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "08d22d8dcdd4ea3dbb3f",
+    "url": "/static/css/main.bdae6e68.chunk.css"
+  },
+  {
+    "revision": "f05fb31dd632a53e71de03736e6f1f2e",
+    "url": "/index.html"
+  }
+];
